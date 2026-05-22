6528604340e138743a3966c153ce28bf960ae347
belen castro <mariabelencastro158@gmail.com>
2026-05-21 22:46:38 -0300

    CasoList hecho

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,31 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> alumnos;
+    
+    public void AgregarAlumno(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+
+    public List<Alumno> RetornarAlumno()
+    {
+        return alumnos;
+    }
+
+    public Alumno? BuscarAlumno( string nombre)
+    {
+        Alumno? alumnoAux = alumnos.Find(x => x.Nombre.Equals(nombre));
+        return alumnoAux;
+    }
+    public void EliminarAlumno(Alumno alumno)
+    {
+        alumnos.Remove(alumno);
+    }
+
+    public void EliminarAlumno(int indice)
+    {
+        alumnos.RemoveAt(indice);
+    }
+
 }
