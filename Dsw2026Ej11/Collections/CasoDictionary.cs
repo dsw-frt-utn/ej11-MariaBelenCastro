b863a7db6e0b649d3670b8df84f0ff1721b2f204
belen castro <mariabelencastro158@gmail.com>
2026-05-21 23:19:25 -0300

    Agrego CasoDictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,54 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private Dictionary<int, Alumno> dictionaryAlumnos = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumno(Alumno alumno, int legajo)
+    {
+        dictionaryAlumnos.Add(legajo, alumno);
+        /* dictionaryAlumnos.TryAdd(legajo, alumno); la diferencia es que este devuelve un bool.
+        En caso de poder insertarlo devuelve un true
+        if (dictionaryAlumnos.TryAdd(legajo, alumno)){
+            manejo de exc
+        }*/
+    }
+
+    public Alumno? BuscarAlumno(int legajo)
+    {
+        //Alumno alumnoAux = dictionaryAlumnos[legajo]; // va directo al legajo que busca.Para busqueda parcial no sirve
+
+        if (dictionaryAlumnos.ContainsKey(legajo))
+        {
+            return dictionaryAlumnos[legajo];
+        }
+        else
+        {
+            return null;
+        }
+
+        /*
+        if (dictionaryAlumnos.TryGetValue(legajo, out var alumnoAux))
+        {
+            return alumnoAux;
+        }
+        else
+        {
+            return null;
+        }*/
+    }
+
+    public Dictionary<int, Alumno> RetornarDiccionario()
+    {
+        return dictionaryAlumnos;
+    }
+
+    public void EliminarAlumno(int legajo)
+    {
+        if (dictionaryAlumnos.ContainsKey(legajo))
+        {
+            dictionaryAlumnos.Remove(legajo);
+        }
+    }
+
+
 }
